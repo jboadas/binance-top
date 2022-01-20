656abd956d3efe043cfb34c276934cc2c6a2fdc5
Jesus Boadas <jboadas@gmail.com>
2022-01-20 09:46:59 -0400

    initial trades download

---
+++ main/top.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+API_URL = 'https://api.binance.com/api/v3/ticker/24hr'
+
+
+def get_trades_from_binance():
+    trades = requests.get(API_URL).json()
+    trades_filter = []
+    for t in trades:
+        if 'USDT' in t['symbol']:
+            trades_filter.append(t)
+    with open('top.json', 'w') as outfile:
+        json.dump(trades_filter, outfile)
+        print('done')
+
+get_trades_from_binance()
