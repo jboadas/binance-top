2dcf76835185f8cc72613e59f07441783e2b6f74
Jesus Boadas <jboadas@gmail.com>
2022-01-23 11:42:19 -0400

    added logging facility

--- main/top.py
+++ main/top.py
@@ -2,8 +2,15 @@ import requests
 import json
 from decimal import Decimal
 import sqlite3
-from collections import OrderedDict
 import datetime
+import sys
+import logging
+
+logging.basicConfig(
+    filename='top.log',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S',)
 
 CON = sqlite3.connect('data/top.db')
 API_URL = 'https://api.binance.com/api/v3/ticker/24hr'
@@ -18,10 +25,10 @@ def get_trades_from_binance():
                 trades_filter.append(t)
         with open('json/top.json', 'w') as outfile:
             json.dump(trades_filter, outfile)
-            print('done')
+            logging.info('get trades done')
             return True
     except Exception as e:
-        print(e)
+        logging.error(e)
         return False
 
 
@@ -42,12 +49,17 @@ def get_top_price_change():
                     top_trades.append(t)
                 if len(top_trades) >= 10:
                     break
-            print("### current top 10 - 24h change")
+            logging.info("### current top 10 - 24h change")
             for t in top_trades:
-                print(t['symbol'], t['priceChangePercent'])
+                logging.info('-' * 20)
+                logging.info("symbol: %s", str(t['symbol']))
+                logging.info("24hr change: %s", str(t['priceChangePercent']))
+                logging.info("askPrice: %s", str(t['askPrice']))
+                logging.info("vwap: %s", str(t['weightedAvgPrice']))
+                logging.info('-' * 20)
             return top_trades
     except Exception as e:
-        print(e)
+        logging.error(e)
         return []
 
 
@@ -98,9 +110,9 @@ def choose_sell_buy(top_change):
         """)
     symbols = cur.fetchall()
     symbol_list = [i[0] for i in symbols]
-    print("### current buys in database")
+    logging.info("### current buys in database")
     for s in symbol_list:
-        print(s)
+        logging.info(s)
     tc_symbol_list = []
     for tc in top_change:
         tc_symbol_list.append(tc['symbol'])
@@ -115,7 +127,7 @@ def choose_sell_buy(top_change):
             if curr_symbol in not_selling:
                 vwap_price = Decimal(t['weightedAvgPrice'])
                 askPrice = Decimal(t['askPrice'])
-                if vwap_price < askPrice:
+                if vwap_price > askPrice:
                     will_sell.append(curr_symbol)
     will_buy_symbols = list(set(tc_symbol_list) - set(symbol_list))
     will_buy = []
@@ -124,12 +136,12 @@ def choose_sell_buy(top_change):
             break
         if tc.get('symbol') in will_buy_symbols:
             will_buy.append(tc)
-    print("### selling:")
+    logging.info("### selling:")
     for ws in will_sell:
-        print(ws)
-    print("### buying:")
+        logging.info(ws)
+    logging.info("### buying:")
     for wb in will_buy:
-        print(wb.get('symbol'))
+        logging.info(wb.get('symbol'))
     sell_selected(will_sell)
     buy_selected(will_buy)
 
@@ -141,9 +153,21 @@ def clean_database():
             delete FROM gainers;
         """)
     CON.commit()
+    logging.info('clean database done')
 
 
-if get_trades_from_binance():
-    top_price_change = get_top_price_change()
-    choose_sell_buy(top_price_change)
-# clean_database()
+def main():
+    if get_trades_from_binance():
+        top_price_change = get_top_price_change()
+        choose_sell_buy(top_price_change)
+
+
+if __name__ == '__main__':
+    if '--clean' in sys.argv:
+        logging.info("cleaning database")
+        clean_database()
+    else:
+        logging.info("#" * 80)
+        logging.info("call main")
+        main()
+        logging.info("#" * 80)
